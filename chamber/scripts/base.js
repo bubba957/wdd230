14f57bdcae3292bd550d9db671874d1554dc5ce4
bubba957 <scody49@gmail.com>
2025-03-17 20:53:10 -0700

    week02

--- chamber/scripts/base.js
+++ chamber/scripts/base.js
@@ -6,3 +6,12 @@ hamBtn.addEventListener('click', () => {
     nav.classList.toggle('open');
     hamBtn.classList.toggle('open');
 });
+
+const optionsY = {year: 'numeric'};
+
+document.querySelector('.year').textContent = new Date().toLocaleDateString('en-US', optionsY);
+
+const optionsDMY = {day: 'numeric', month: 'short', year: 'numeric'};
+
+document.querySelector('.lastModified').textContent = new Date().toLocaleDateString('en-US', optionsDMY);
+
